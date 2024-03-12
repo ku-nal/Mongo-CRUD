bb2e39cf60de75f77e70bb29ab922e4d2c53ae58
kunal <kn362288@dal.ca>
2024-03-12 07:29:16 +0530

    Updated code

--- user-controller.js
+++ user-controller.js
@@ -1,61 +1,101 @@
-const {getClient} = require('./connectDB');
-const User = require('./user-model');
-const SuccessResponse = require('./success-response');
-const ErrorResponse = require('./error-response');
+const { getClient } = require("./connectDB");
+const User = require("./user-model");
+const SuccessResponse = require("./success-response");
+const ErrorResponse = require("./error-response");
 
-class UserController{
-    async createUser(req,res){
-        try{
-            const response = await User.create({
-                email: req.body.email,
-                firstName: req.body.firstName
-            });
-            SuccessResponse.message = "User added";
-            return res.status(200).json(SuccessResponse);
+class UserController {
+  async createUser(req, res) {
+    try {
+      if (req.body.email && req.body.firstName) {
+        const response = await User.create({
+          email: req.body.email,
+          firstName: req.body.firstName,
+        });
+        SuccessResponse.message = "User added";
+        return res.status(200).json(SuccessResponse);
+      } else {
+        console.log(req.firstName);
+        if (!req.body.email && !req.body.firstName) {
+          ErrorResponse.message =
+            "email and firstName should be present in the request body";
+          return res.status(400).json(ErrorResponse);
+        } else if (!req.body.email) {
+          ErrorResponse.message = "email should be present in the request body";
+          return res.status(400).json(ErrorResponse);
+        } else {
+          ErrorResponse.message =
+            "firstName should be present in the request body";
+          return res.status(400).json(ErrorResponse);
         }
-        catch(error){
-            return res.status(400).json(ErrorResponse);
-        }
+      }
+    } catch (error) {
+      ErrorResponse.message = "Error creating a new user";
+      return res.status(500).json(ErrorResponse);
     }
+  }
 
-    async getAll(req,res){
-        try{
-            const users = await User.find({}).select("email firstName id");
-            SuccessResponse.message = "Users retreived";
-            SuccessResponse.users = users;
+  async getAll(req, res) {
+    try {
+      const users = await User.find({}).select("email firstName id");
+      SuccessResponse.message = "Users retreived";
+      SuccessResponse.users = users;
 
-            return res.status(200).json(SuccessResponse);
-        }
-        catch(error){
-            return res.status(400).json(ErrorResponse);
-        }
+      return res.status(200).json(SuccessResponse);
+    } catch (error) {
+      return res.status(400).json(ErrorResponse);
     }
+  }
 
-    async get(req,res){
-        try{
-            const user = await User.findOne({_id: req.params.id}).select("email firstName id");
+  async get(req, res) {
+    try {
+      var user = await User.findOne({ _id: req.params.id }).select(
+        "email firstName id"
+      );
+      return res.status(200).json({
+        success: true,
+        user: user,
+      });
+    } catch (error) {
+        if(!user){
             return res.status(200).json({
                 success: true,
-                user: user
-            });
+                user:{}
+            })
         }
-        catch(error){
-            return res.status(400).json(ErrorResponse);
-        }
+      return res.status(400).json(ErrorResponse);
     }
+  }
 
-    async updateUser(req,res){
-        try{
-            console.log(req.params.id, req.body);
-            const response = await User.findOneAndUpdate({_id: req.params.id}, req.body);
-            console.log(response);
-            SuccessResponse.message = "User updated";
-            return res.status(200).json(SuccessResponse);
+  async updateUser(req, res) {
+    try {
+      if (req.body.email && req.body.firstName) {
+        console.log(req.params.id, req.body);
+        const response = await User.findOneAndUpdate(
+          { _id: req.params.id },
+          req.body
+        );
+        console.log(response);
+        SuccessResponse.message = "User updated";
+        return res.status(200).json(SuccessResponse);
+      } else {
+        if (!req.body.email && !req.body.firstName) {
+          ErrorResponse.message =
+            "email and firstName should be present in the request body";
+          return res.status(400).json(ErrorResponse);
+        } else if (!req.body.email) {
+          ErrorResponse.message = "email should be present in the request body";
+          return res.status(400).json(ErrorResponse);
+        } else {
+          ErrorResponse.message =
+            "firstName should be present in the request body";
+          return res.status(400).json(ErrorResponse);
         }
-        catch(error){
-            return res.status(400).json(ErrorResponse);
-        }
+      }
+    } catch (error) {
+        console.log(error);
+      return res.status(500).json(ErrorResponse);
     }
+  }
 }
 
 module.exports = UserController;
